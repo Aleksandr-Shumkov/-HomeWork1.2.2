e6eff38736d34259e213aef8068d970a2302e4f5
Shumkov Aleksandr <aleksandr.shymkov@gmail.com>
2023-01-12 13:34:48 +0700

    Initial commit

---
+++ src/main/kotlin/ru/netology/Main.kt
@@ -0,0 +1,26 @@
+package ru.netology
+
+fun main() {
+    var likes = 1
+
+    while (likes < 212) {
+        println("Понравилось " + likes + " " + getTextEnding(likes))
+        likes += 1
+    }
+}
+
+fun getTextEnding(likes:Int): String {
+
+    val remainder10 = likes % 10;
+    val remainder100 = ((likes - remainder10) / 10) % 10;
+
+    if (remainder100 != 1) {
+        return when (likes % 10) {
+            1 -> "человеку"
+            else -> "людям"
+        }
+    }
+
+    return "людям"
+
+}
